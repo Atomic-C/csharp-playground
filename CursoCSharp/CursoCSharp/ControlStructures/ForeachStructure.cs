68000d1e03d7e75ff779b99c78b44b8ec6a205be
Pedro Sousa <pedro.p.sousa@sapo.pt>
2022-03-10 22:51:47 +0000

    Foreach control structure.

---
+++ CursoCSharp/CursoCSharp/ControlStructures/ForeachStructure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.ControlStructures
+{
+    public class ForeachStructure
+    {
+        public static void Execute()
+        {
+            string word = "Hello!";
+            foreach (var item in word)
+            {
+                Console.WriteLine(item);
+            }
+
+            string students = new string[] { "Pedro", "Ana", "Harry" };
+            foreach (var item in students)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -30,6 +30,7 @@ namespace CursoCSharp {
                 {"Estrutura While - Estruturas de Controlo", WhileStructure.Execute },
                 {"Estrutura Do While - Estruturas de Controlo", DoWhileStructure.Execute },
                 {"Estrutura For - Estruturas de Controlo", ForStructure.Execute },
+                {"Estrutura Foreach - Estruturas de Controlo", ForeachStructure.Execute },
             }) ;
 
             central.SelecionarEExecutar();
